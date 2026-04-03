7c0a3a9b9b46b31904ac4000af5e62b44e19f47b
Lera <lera@example.com>
2026-04-03 20:49:15 +0300

    Form added products

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = '123'
+products = []
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        price = request.form.get('price')
+        category = request.form.get('category')
+
+        for item in products:
+            if item.get('name') == name:
+                flash('Такий товар вже є!')
+                break
+        else:
+            info_product = {'name': name, 'price': price, 'category': category}
+            products.append(info_product)
+
+        return redirect(url_for('index'))
+
     return render_template('index.html')
 
 
