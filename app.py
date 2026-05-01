7536ca7b9d51c67b3836a71db7379b275e8b2bb4
Lera <lera@example.com>
2026-05-01 18:15:31 +0300

    Starter

--- app.py
+++ app.py
@@ -14,5 +14,6 @@ init_db()
 app.register_blueprint(product_bp)
 app.register_blueprint(auth_bp)
 
-port=int(os.environ.get("PORT",5000))
-app.run(host="0.0.0.0",port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
